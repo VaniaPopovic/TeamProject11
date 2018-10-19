42a977180cc91bd14f49d4acae8f16215890f881
Charles Ren <chongbin@CharlesRen-mac.local>
2018-10-19 21:14:34 +0100

    cloud database created
    I have created a cloud database and you guys can just update the project from github then if you run the project you will connect to the cloud server directly.

--- server/config.js
+++ server/config.js
@@ -1,5 +1,6 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
+  // mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
+  mongoURL: process.env.MONGO_URL || 'mongodb+srv://charles:6670517@cluster0-oaiei.gcp.mongodb.net/project11?retryWrites=true',
   port: process.env.PORT || 8000,
 };
 
---
+++ server/serverReadme.md
@@ -0,0 +1,13 @@
+#### config.js
+  set the mongodb's url
+  
+#### package.js
+devdependence :
+'nodemon' is monitoring all the code changes and restart auto
+
+#### server.js
+
+ Babel converts **import** and **export** declaration to CommonJS (require/module.exports) by default.
+
+
+one document is controlled by one proccedure
