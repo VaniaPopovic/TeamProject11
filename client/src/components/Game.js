8eeae69f6277af421f9b97b1831ff2cc76f74fd7
Vanja Popovic <vaniapopovic@icloud.com>
2018-11-27 21:58:50 +0000

    time control for each puzzle

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import BoxGrid from "./BoxGrid";
 import WordList from "./WordList";
+import { Button, Card, CardBody, CardHeader, Col, Modal, ModalBody, ModalFooter, ModalHeader, Row } from 'reactstrap';
 
 class Game extends Component {
   constructor(props) {
@@ -8,21 +9,53 @@ class Game extends Component {
     this.state = {
       data: props.data,
       puzzleIndex: 0,
+      time: new Date().getTime(),
+      elapsedTime: 0,
       squares: this.getSquares(props.data[0].grid),
       answers: this.getAnswers(props.data[0].answers),
+      warning: false,
+      timeTaken: "",
       target: {}
     };
+    this.toggleWarning = this.toggleWarning.bind(this);
   }
-
+  componentDidUpdate(prevProps) {
+    // Typical usage (don't forget to compare props):
+    //if(this.isPuzzleDone()) {
+    //console.log("HELLOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+   // }
+    }
+  toggleWarning() {
+    this.setState({
+      warning: !this.state.warning,
+    });
+  }
   setNextPuzzle() {
     const index = (this.state.puzzleIndex + 1) % this.state.data.length;
     const data = this.state.data[index];
+    console.log(data);
     this.setState({
       puzzleIndex: index,
-      squares: this.getSquares(data.character_grid),
+      time: new Date().getTime(),
+      squares: this.getSquares(data.grid),
       answers: this.getAnswers(data.answers),
+      warning: true,
       target: {}
     });
+  }
+  isPuzzleDone() {
+    // Check if we've found all targets.
+    for(let answer of this.state.answers) {
+      if(!answer.isFound) {
+        return;
+      }
+    }
+    var finish = new Date().getTime();
+    var t = finish -this.state.time;
+    this.setState({elapsedTime: t})
+    setTimeout(function() {
+      this.setNextPuzzle();
+    }.bind(this), 2500);
   }
   getAnswers(squareData) {
     var answers = [];
@@ -87,6 +120,7 @@ class Game extends Component {
           n[i].isFound = true;
 
           this.setState({ answers: n });
+          this.isPuzzleDone();
         }
 
         // // Update state with revealed squares.
@@ -97,21 +131,39 @@ class Game extends Component {
     }
     console.log("Target", revealedTarget);
   }
-
+  onSave(totalTime){
+    console.log("TIME TAKEN ", totalTime)
+  }
   render() {
     var answers = this.state.answers;
+    var time = Date;
     console.log("render", answers);
+    console.log("TIME",this.state.time);
     return (
       <div className="game-container">
+      <h1>LEVEL {this.state.puzzleIndex +1}</h1>
         <WordList answers={answers} />
         <div className="game-board">
           <BoxGrid
             squares={this.state.squares}
             onMakeSelection={this.handleMakeSelection.bind(this)}
           />
+        
         </div>
+                <Modal isOpen={this.state.warning} toggle={this.toggleWarning}
+                       className={'modal-warning ' + this.props.className}>
+                  <ModalHeader toggle={this.toggleWarning}>Modal title</ModalHeader>
+                  <ModalBody>
+                    TIME TAKEN :{this.state.elapsedTime} MILISECONDS
+                  </ModalBody>  
+                  <ModalFooter>
+                    <Button color="warning" onClick={this.toggleWarning}>Do Something</Button>{' '}
+                    <Button color="secondary" onClick={this.toggleWarning}>Cancel</Button>
+                  </ModalFooter>
+                </Modal>
       </div>
     );
+    
   }
 }
 
